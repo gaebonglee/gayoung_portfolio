c90248cac4d036c9edf264fef14867f192d4d050
gayoung lee <ken604@naver.com>
2024-08-04 15:24:23 +0900

    텍스트 디자인 완성

--- src/components/me/Info.tsx
+++ src/components/me/Info.tsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../style/desktop/me/Info.scss";
 
 const Info: React.FC = () => {
+  const [content, setContent] = useState("");
+  const [index, setIndex] = useState(0);
+
+  const errorHTML = `<div className="info">
+            <h3>LEE GA YOUNG</h3>
+            <p>1998.01.21</p>
+            <p>Department of stereoscopic molding at Kyonggi University</p>
+            <p>2gayoung.2da@gmail.com</p>
+          </div>`;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => {
+        if (prevIndex < errorHTML.length) {
+          setContent(errorHTML.slice(0, prevIndex + 1) + "|");
+          return prevIndex + 1;
+        } else {
+          clearInterval(interval);
+          setContent(errorHTML);
+          return prevIndex;
+        }
+      });
+    }, 10);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className="infoSection">
       <div className="infoContainer">
         <div className="infoText_wrap">
-          <div className="info name">
-            <a>LEE GA YOUNG</a>
-            <a className="age">1998.01.21</a>
-          </div>
-          <div className="info univ">
-            <a>Department of stereoscopic molding at Kyonggi University</a>
-          </div>
-          <div className="info email">
-            <a>2gayoung.2da@gmail.com</a>
-          </div>
+          <div dangerouslySetInnerHTML={{ __html: content }}></div>
         </div>
         <svg className="lineShape topLine" viewBox="0 0 200 50">
           <polyline points="0,50 50,0 200,0" />
