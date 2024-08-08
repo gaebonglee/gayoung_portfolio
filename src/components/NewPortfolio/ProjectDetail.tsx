d876b58d24006c150bb0eb9ff786bb4ea4cc60ca
gayoung lee <ken604@naver.com>
2024-08-09 03:03:22 +0900

    디자인 애니메이션 추가 필요

--- src/components/NewPortfolio/ProjectDetail.tsx
+++ src/components/NewPortfolio/ProjectDetail.tsx
@@ -9,50 +9,56 @@ interface ProjectDetailProps {
 
 const ProjectDetail: React.FC<ProjectDetailProps> = ({ project, onClose }) => {
   return (
-    <div className="project_detail">
-      <button onClick={onClose}>Close</button>
-      <div className="cardContent">
-        <div className="myWork_wrap">
-          <div className={`myWork ${project.design ? "true" : "false"}`}>
-            <a>Design</a>
+    <div className="back">
+      <div className="project_detail">
+        <div className="closeBtn">
+
+          <button onClick={onClose}>Close</button>
+        </div>
+
+        <div className="cardContent">
+          <div className="myWork_wrap">
+            <div className={`myWork ${project.design ? "true" : "false"}`}>
+              <a>Design</a>
+            </div>
+            <div className={`myWork ${project.frontEnd ? "true" : "false"}`}>
+              <a>Front-end</a>
+            </div>
+            <div className={`myWork ${project.backEnd ? "true" : "false"}`}>
+              <a>Back-end</a>
+            </div>
           </div>
-          <div className={`myWork ${project.frontEnd ? "true" : "false"}`}>
-            <a>Front-end</a>
+          <div className="titleWrap">
+            <p>{project.title}</p>
+            <a>{project.subTitle}</a>
           </div>
-          <div className={`myWork ${project.backEnd ? "true" : "false"}`}>
-            <a>Back-end</a>
+          <div className="description">
+            <p>{project.description}</p>
           </div>
-        </div>
-        <div className="titleWrap">
-          <p>{project.title}</p>
-          <a>{project.subTitle}</a>
-        </div>
-        <div className="description">
-          <p>{project.description}</p>
-        </div>
-        <div className="card_inner">
-          <h4>Period</h4>
-          <a>{project.period}</a>
-        </div>
-        <div className="card_inner">
-          <h4>Tech</h4>
-          <a> {project.tech}</a>
-        </div>
-        <div className="linkWrap">
-          <a
-            href={project.projectLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Demo
-          </a>
-          <a
-            href={project.githubLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source
-          </a>
+          <div className="card_inner">
+            <h4>Period</h4>
+            <a>{project.period}</a>
+          </div>
+          <div className="card_inner">
+            <h4>Tech</h4>
+            <a> {project.tech}</a>
+          </div>
+          <div className="linkWrap">
+            <a
+              href={project.projectLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Demo
+            </a>
+            <a
+              href={project.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Source
+            </a>
+          </div>
         </div>
       </div>
     </div>
