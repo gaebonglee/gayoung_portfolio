fde3e4dfd3fd85fd0b53808b55b8f642c014a7c3
gayoung lee <ken604@naver.com>
2024-08-08 15:41:54 +0900

    projects 타이틀 단어 수정 필요

--- src/pages/NewPortfolio/Projects.tsx
+++ src/pages/NewPortfolio/Projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../style/pages/Projects.scss";
+import "animate.css";
 
 interface ProjectsProps {
   show: boolean;
@@ -8,7 +9,17 @@ interface ProjectsProps {
 const Projects: React.FC<ProjectsProps> = ({ show }) => {
   return (
     <section className={`projectSection ${show ? "slide-in" : ""}`}>
-      <div></div>
+      <div className="ProjectTitle">
+
+        <h1
+          className={`animate__animated ${
+            show ? "animate__fadeInUpBig animate-delay-1s" : ""
+          }`}
+        >
+          Projects
+        </h1>
+        
+      </div>
     </section>
   );
 };
