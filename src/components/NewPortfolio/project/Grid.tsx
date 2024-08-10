d813a99241cd377a45ea9b4aa205fe58953e75ed
gayoung lee <ken604@naver.com>
2024-08-10 23:09:19 +0900

    진짜 거의 다 왔당!!@~!

--- src/components/NewPortfolio/project/Grid.tsx
+++ src/components/NewPortfolio/project/Grid.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../../style/NewPortfolio/project/Grid.scss";
 import { projects, ProjectType } from "../../../data/projects";
 import ProjectDetail from "./ProjectDetail";
@@ -9,101 +9,110 @@ const Grid: React.FC = () => {
   const [selectedProject, setSelectedProject] = useState<ProjectType | null>(
     null
   );
+  const [showDetail, setShowDetail] = useState(false);
 
-  const handleCardClick = (project: ProjectType, index: number) => {
-    setSelectedProject({ ...project, index });
-    showContent(index);
+  useEffect(() => {
+    showGridImg();
+  }, []);
 
-    const gridCells = document.querySelectorAll(".grid__cell");
-    gridCells.forEach((cell, cellIndex) => {
-      if (cellIndex !== index) {
-        cell.classList.add("grid__cell--hidden");
+  const showGridImg = () => {
+    const gridCells = document.querySelectorAll(
+      ".grid__cell"
+    ) as NodeListOf<HTMLElement>;
+
+    gsap.fromTo(
+      gridCells,
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        ease: "power2.out",
+        stagger: {
+          each: 0.1,
+          from: "random",
+        },
       }
+    );
+    setShowDetail(false);
+  };
+
+  const closeGridImg = (index: number) => {
+    const gridCells = document.querySelectorAll(
+      ".grid__cell"
+    ) as NodeListOf<HTMLElement>;
+
+    gsap.to(gridCells, {
+      duration: 1,
+      opacity: 0,
+      y: 40,
+      ease: "power1.inOut",
+      stagger: {
+        amount: 1,
+        from: "center",
+        grid: "auto",
+      },
+      onComplete: () => {
+        setShowDetail(true);
+        showContent(index);
+      },
     });
   };
 
   const showContent = (index: number) => {
-    const cellElement = document.querySelector(
-      `.grid__cell-img-${index}`
-    ) as HTMLElement | null;
-    const cellInnerElement = document.querySelector(
-      `.grid__cell-img-inner-${index}`
-    ) as HTMLElement | null;
-
-    if (!cellElement || !cellInnerElement) {
-      console.error("Element not found");
-      return;
-    }
-
-    const targetX = window.innerWidth * 0.8;
-    const targetY = window.innerHeight * 0.2;
-
-    // 현재 카드의 중앙 좌표
-    const cardRect = cellElement.getBoundingClientRect();
-    const currentX = cardRect.left + cardRect.width / 2;
-    const currentY = cardRect.top + cardRect.height / 2;
-
-    // 이동해야 할 거리 계산
-    const x = targetX - currentX;
-    const y = targetY - currentY;
-
-    const scale = 1.5; 
-
-    gsap
-      .timeline({
-        defaults: {
-          duration: 1.2,
-          ease: "expo.inOut",
+    gsap.fromTo(
+      ".content",
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: "power2.out",
+        onComplete: () => {
+          document.querySelector(".content")?.classList.add("visible");
         },
-      })
-      .to(cellElement, { scale, x, y })
-      .to(cellInnerElement, { scale: 1 }, 0)
-      .to(".grid", { pointerEvents: "none" }, 0)
-      .to(".content", { opacity: 1, pointerEvents: "auto" }, "-=0.8");
+      }
+    );
+  };
+
+  const handleCardClick = (project: ProjectType, index: number) => {
+    setSelectedProject({ ...project, index });
+    closeGridImg(index);
   };
 
   const closeContent = () => {
     if (!selectedProject) return;
-    const gridCells = document.querySelectorAll(".grid__cell");
-    gridCells.forEach((cell) => {
-      cell.classList.remove("grid__cell--hidden");
+
+    gsap.to(".content", {
+      opacity: 0,
+      duration: 1,
+      ease: "power1.inOut",
+      onComplete: () => {
+        setSelectedProject(null);
+        setShowDetail(false);
+        showGridImg();
+      },
     });
-
-    gsap
-      .timeline({
-        defaults: {
-          duration: 1,
-          ease: "expo.inOut",
-        },
-      })
-      .to(".content", { opacity: 0, pointerEvents: "none" })
-      .to(".grid", { pointerEvents: "auto" }, 0)
-      .to(
-        `.grid__cell-img-${selectedProject.index}`,
-        { scale: 1, x: 0, y: 0 },
-        0
-      );
-
-    setSelectedProject(null);
   };
 
   return (
     <section className="gallery">
-      <div className="grid grid--large">
-        {projects.map((project, index) => (
-          <ImageCell
-            key={index}
-            project={project}
-            index={index}
-            onClick={() => handleCardClick(project, index)}
-          />
-        ))}
-      </div>
-      <div className="content">
-        {selectedProject && (
+      {!showDetail ? (
+        <div className="grid grid--large">
+          {projects.map((project, index) => (
+            <ImageCell
+              key={index}
+              project={project}
+              index={index}
+              onClick={() => handleCardClick(project, index)}
+            />
+          ))}
+        </div>
+      ) : (
+        selectedProject && (
           <ProjectDetail project={selectedProject} onClose={closeContent} />
-        )}
-      </div>
+        )
+      )}
     </section>
   );
 };
